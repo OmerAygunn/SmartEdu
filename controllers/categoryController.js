75fa49460db5289b901383d3ffe076ce930f482d
OmerAyg <omeraygun438@gmail.com>
2024-03-12 15:25:46 +0300

    Implemented category addition and deletion functionality

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,10 +3,8 @@ const Category = require('../models/Category')
 exports.createCategory = async (req,res) => {
    try{
     const category = await Category.create(req.body)
-    res.status(201).json({
-        status:'success',
-        category,
-    })
+    res.status(200).redirect('/users/dashboard');
+
    }
    catch(error){
        res.status(400).json({
@@ -15,3 +13,17 @@ exports.createCategory = async (req,res) => {
        })
    }
 }
+
+exports.deleteCategory = async (req, res) => {
+    try {    
+  
+      await Category.findByIdAndDelete(req.params.id)
+      res.status(200).redirect('/users/dashboard');
+  
+    } catch (error) {
+      res.status(400).json({
+        status: 'fail',
+        error,
+      });
+    }
+  };
